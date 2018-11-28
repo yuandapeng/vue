41abf80adac5aacb9788f1891ce60b942247563c
yuanpp <yuanpp.xs@inesa-it.com>
2018-11-28 16:20:40 +0800

    -add menu

---
+++ src/models/permission.js
@@ -0,0 +1,33 @@
+import {  RouterMap as constantRouterMap } from '@/router'
+
+/**
+ * 通过meta.role判断是否与当前用户权限匹配
+ * @param roles
+ * @param route
+ */
+function hasPermission(roles, route) {
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+
+
+const permission = {
+  state: {
+    routers: constantRouterMap,
+    addRouters: []
+  },
+  mutations: {
+    SET_ROUTERS: (state, routers) => {
+      state.addRouters = routers
+      state.routers = constantRouterMap.concat(routers)
+    }
+  },
+  actions: {
+
+  }
+}
+
+export default permission
